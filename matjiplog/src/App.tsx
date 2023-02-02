d97774f18f12db5ea6640c3ca418809c02b38cd8
5469804 <92029126+5469804@users.noreply.github.com>
2023-02-02 17:11:09 +0900

    [FEAT] 회원가입 페이지 레이아웃 및 스타일 적용 완료

--- matjiplog/src/App.tsx
+++ matjiplog/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
+import SignUp from "./Pages/SignUp";
 
 import GlobalStyle from "./styles/common/global";
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<>Main</>}></Route>
           <Route path="/login" element={<Login/>}></Route>
+          <Route path="/signup" element={<SignUp/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
